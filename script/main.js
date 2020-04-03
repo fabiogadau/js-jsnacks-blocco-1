2936a961924a055dcd6df128f1687884be5e5346
Fabio Gadau <fabiogadau92@gmail.com>
2020-04-03 15:01:11 +0200

    first commit

---
+++ script/main.js
@@ -0,0 +1,20 @@
+console.log('Js ok');
+/*
+* Snack 1
+L’utente inserisce due parole in successione, con due prompt. Il software stampa la parola più lunga. Utilizza una funzione per determinare e ritornare la parola più lunga.
+*/
+
+/*
+* Snack 2
+Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo.
+*/
+
+/*
+* Snack 3
+Generatore di “nomi cognomi” casuali: prendendo una lista di nomi (‘Michele’, ‘Fabio’, ‘Roberto’) e una lista di cognomi (‘Forghieri’, ‘Papagni’, ‘Marazzini’), Gatsby vuole generare una falsa lista di 5 invitati.
+*/
+
+/*
+* Snack 4
+Crea un array di numeri interi, ad esempio (1, 4, 2, 15, 120, 17, 3) e fai la somma di tutti gli elementi che sono in posizione dispari. Prova prima senza funzione e poi con funzione.
+*/
